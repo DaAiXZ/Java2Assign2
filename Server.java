57064cae9c4077af8e01cc633a5adace488c0c0b
DaAiXZ <93817105+DaAiXZ@users.noreply.github.com>
2022-11-22 21:48:18 +0800

    Update Server.java

--- Server.java
+++ Server.java
@@ -6,17 +6,17 @@ import java.util.List;
 
 public class Server {
 
-    private static final int PORT = 8888;
-    public static List<ServerThread> clients = new ArrayList<>();
-    public static List<List<ServerThread>> rooms= new ArrayList<>();
+  private static final int PORT = 8888;
+  public static List<ServerThread> clients = new ArrayList<>();
+  public static List<List<ServerThread>> rooms = new ArrayList<>();
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        ServerSocket serverSocket = new ServerSocket(PORT);
-        while (true) {
-            ServerThread serverThread = new ServerThread(serverSocket.accept(), clients.size());
-            clients.add(serverThread);
-            serverThread.start();
-        }
+    ServerSocket serverSocket = new ServerSocket(PORT);
+    while (true) {
+      ServerThread serverThread = new ServerThread(serverSocket.accept(), clients.size());
+      clients.add(serverThread);
+      serverThread.start();
     }
+  }
 }
