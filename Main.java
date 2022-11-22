bbf5b2540aa03c399be3fce9d12e36d69f795666
DaAiXZ <93817105+DaAiXZ@users.noreply.github.com>
2022-11-22 21:45:49 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,27 +8,27 @@ import javafx.stage.Stage;
 
 public class Main extends Application {
 
-    @Override
+  @Override
     public void start(Stage primaryStage) {
-        try {
-            FXMLLoader fxmlLoader = new FXMLLoader();
+    try {
+      FXMLLoader fxmlLoader = new FXMLLoader();
 
-            fxmlLoader.setLocation(getClass().getClassLoader().getResource("mainUI.fxml"));
-            Pane root = fxmlLoader.load();
-            primaryStage.setTitle("Tic Tac Toe");
-            primaryStage.setOnCloseRequest(event -> {
-                System.exit(0);
-            });
+      fxmlLoader.setLocation(getClass().getClassLoader().getResource("mainUI.fxml"));
+      Pane root = fxmlLoader.load();
+      primaryStage.setTitle("Tic Tac Toe");
+      primaryStage.setOnCloseRequest(event -> {
+        System.exit(0);
+      });
 
-            primaryStage.setScene(new Scene(root));
-            primaryStage.setResizable(false);
-            primaryStage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+      primaryStage.setScene(new Scene(root));
+      primaryStage.setResizable(false);
+      primaryStage.show();
+    } catch (Exception e) {
+      e.printStackTrace();
     }
+  }
 
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 }
